db42d7cf63e876986cd9b0e4d7edc2fe485a1b0c
leo <leoguerra.gm@gmail.com>
2023-10-22 18:29:01 -0300

    update

---
+++ src/LifeDrop/Migrations/20231022212300_M01-AddTableBancoDeSangue.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LifeDrop.Migrations
+{
+    public partial class M01AddTableBancoDeSangue : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
