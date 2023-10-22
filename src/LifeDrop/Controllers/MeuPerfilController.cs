b0acc674e08684cbb4fd689f293faa654d64ed37
Natali Costa <natalixcosta@icloud.com>
2023-10-22 15:21:01 -0300

    Adicionando integração do front com back da tela "Meu perfil".

--- src/LifeDrop/Controllers/MeuPerfilController.cs
+++ src/LifeDrop/Controllers/MeuPerfilController.cs
@@ -4,22 +4,68 @@ using Microsoft.EntityFrameworkCore;
 
 namespace LifeDrop.Controllers
 {
-    public class MeuPerfilController : Controller        
+    public class MeuPerfilController : Controller
     {
         private readonly AppDbContext _context;
-        private int idUsuario = 2;
+        private readonly int _idUsuarioLogado = 3;
+        private readonly Usuario _usuarioLogado;
 
-        public  MeuPerfilController(AppDbContext context)
+        public MeuPerfilController(AppDbContext context)
         {
             _context = context;
+            _usuarioLogado = _context.Usuarios.FirstOrDefault(x => x.IdUsuario == _idUsuarioLogado);
         }
 
         public IActionResult Index()
         {
-            
-            var doador = _context.Doadores.FirstOrDefault(x => x.IdUsuario == idUsuario);
+            var doador = _context.Doadores.FirstOrDefault(x => x.IdUsuario == _idUsuarioLogado);
+
+            if (doador == null)
+            {
+                doador = new Doador
+                {
+                    IdUsuario = _usuarioLogado.IdUsuario,
+                    Nome = _usuarioLogado.Nome,
+                    Email = _usuarioLogado.Email
+                };
+            }
 
             return View(doador);
+        }
+
+        public async Task<IActionResult> Salvar(int idDoador, Doador doador)
+        {
+            if (ModelState.IsValid)
+            {
+                if (doador.IdDoador > 0 && doador.IdDoador == idDoador)
+                {
+                    Doador doadorExistente = _context.Doadores.Find(idDoador);
+                    doadorExistente.Nome = doador.Nome;
+                    doadorExistente.Email = doador.Email;
+                    doadorExistente.Telefone = doador.Telefone;
+                    doadorExistente.CPF = doador.CPF;
+                    doadorExistente.DataNasc = doador.DataNasc;
+                    doadorExistente.Endereco = doador.Endereco;
+                    doadorExistente.Genero = doador.Genero;
+                    doadorExistente.TipoSanguineo = doador.TipoSanguineo;
+
+                    _context.Doadores.Update(doadorExistente);
+
+                    _usuarioLogado.Nome = doador.Nome;
+                    _context.Usuarios.Update(_usuarioLogado);
+
+                    await _context.SaveChangesAsync();
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    _context.Doadores.Add(doador);
+                    await _context.SaveChangesAsync();
+                    return RedirectToAction("Index");
+                }
+            }
+
+            return View();
         }
     }
 }
---
+++ src/LifeDrop/Migrations/20231020211818_M03-AlterTableDoadores.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LifeDrop.Migrations
+{
+    public partial class M03AlterTableDoadores : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Nome",
+                table: "Doadores",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Email",
+                table: "Doadores",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
