de12c7aff992fd2b8933553c8ad5352113dc9f32
leo <leoguerra.gm@gmail.com>
2023-11-18 20:48:48 -0300

    at table

---
+++ src/LifeDrop/Migrations/20231118164707_Relacao.cs
@@ -0,0 +1,95 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LifeDrop.Migrations
+{
+    public partial class Relacao : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "AB_Negative",
+                table: "BancosDeSangue",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "AB_Positive",
+                table: "BancosDeSangue",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "A_Negative",
+                table: "BancosDeSangue",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "A_Positive",
+                table: "BancosDeSangue",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "B_Negative",
+                table: "BancosDeSangue",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "B_Positive",
+                table: "BancosDeSangue",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "O_Negative",
+                table: "BancosDeSangue",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "O_Positive",
+                table: "BancosDeSangue",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AB_Negative",
+                table: "BancosDeSangue");
+
+            migrationBuilder.DropColumn(
+                name: "AB_Positive",
+                table: "BancosDeSangue");
+
+            migrationBuilder.DropColumn(
+                name: "A_Negative",
+                table: "BancosDeSangue");
+
+            migrationBuilder.DropColumn(
+                name: "A_Positive",
+                table: "BancosDeSangue");
+
+            migrationBuilder.DropColumn(
+                name: "B_Negative",
+                table: "BancosDeSangue");
+
+            migrationBuilder.DropColumn(
+                name: "B_Positive",
+                table: "BancosDeSangue");
+
+            migrationBuilder.DropColumn(
+                name: "O_Negative",
+                table: "BancosDeSangue");
+
+            migrationBuilder.DropColumn(
+                name: "O_Positive",
+                table: "BancosDeSangue");
+        }
+    }
+}
